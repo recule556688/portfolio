86e256d1aab4ab85a1d6ebc8ede3699af09e846d
Karma <ledombre@gmail.com>
2024-08-31 17:25:14 +0200

    changed date

--- app/page.tsx
+++ app/page.tsx
@@ -329,7 +329,7 @@ export default function Portfolio() {
         className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-gray-700 bg-gray-800"
       >
         <p className="text-xs text-gray-400">
-          © 2023 Karma. All rights reserved.
+          © 2024 Tess Corp. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
